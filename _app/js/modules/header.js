f6bffc2fc4e2d1fc8a37902b5bcbff16b2186ee0
Darin Hajou <it_related@hotmail.com>
2023-05-21 18:49:18 +0200

    Improve responsiveness of header search bar

--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -2,37 +2,58 @@ export default function () {
 	const searchIcon = document.querySelector('.search-icon');
 	const searchClose = document.querySelector('.search-close');
 	const searchBar = document.querySelector('.header__search input[type="search"]');
+	const searchBarIcon = document.querySelector('.search-bar-icon');
  
-	searchIcon.addEventListener('click', function() {
-		 // Only show the search bar if screen width is less than 480px
-			  this.style.display = 'none';
-			  searchClose.style.display = 'block';
-			  searchBar.style.display = 'block';
-			  searchBar.classList.add('center-search');  // Add the new CSS class
-	});
-	
-	searchClose.addEventListener('click', function() {
-		 // Only hide the search bar if screen width is less than 480px
-			  this.style.display = 'none';
-			  searchIcon.style.display = 'block';
-			  searchBar.style.display = 'none';
-			  searchBar.classList.remove('center-search');  // Removes the CSS class
-	});
-
-	// When the window is resized, check if we need to show the search icon
-	window.addEventListener('resize', function() {
-		const menuButton = document.querySelector('.header__menu-button');
-		 if (window.matchMedia('(min-width:1240px)').matches) {
-			  searchIcon.style.display = 'none';
-			  searchBar.style.display = 'block';
+	function handleResize() {
+	  if (window.matchMedia('(min-width:1024px)').matches) {
+		 // Screen is wider than 1024px, search bar should always be open
+		 searchBar.style.display = '';
+		 searchClose.style.display = 'none';
+		 searchIcon.style.display = 'none';
+		 searchBarIcon.style.display = 'block';
+	  } else {
+		 // Screen is narrower than 1024px
+		 if (searchBar.style.display === 'block') {
+			// Search bar is open
+			searchClose.style.display = 'block';
+			searchIcon.style.display = 'none';
+			searchBarIcon.style.display = 'block';
 		 } else {
-			  searchIcon.style.display = 'block';
-			  searchClose.style.display = 'none';
-			  searchBar.style.display = 'none';
+			// Search bar is closed
+			searchClose.style.display = 'none';
+			searchIcon.style.display = 'block';
+			searchBarIcon.style.display = 'none';
 		 }
+	  }
+	}
+ 
+	// Initialize visibility of search bar icon based on window size
+	handleResize();
+ 
+	searchIcon.addEventListener('click', function () {
+	  this.style.display = 'none'; // Hide the search icon
+	  searchClose.style.display = 'block'; // Show the close button
+	  searchBar.style.display = 'block'; // Show the search bar
+	  searchBarIcon.style.display = 'block'; // Show the search-bar-icon
+	  handleResize(); // Update the visibility of the search icon on resize
 	});
-
-	document.querySelector('.header__menu-button').addEventListener('click', function() {
-		 document.querySelector('.header__nav').classList.toggle('header__nav--active');
+ 
+	searchClose.addEventListener('click', function () {
+	  this.style.display = 'none'; // Hide the close button
+	  searchIcon.style.display = 'block'; // Show the search icon
+	  searchBar.style.display = 'none'; // Hide the search bar
+	  searchBarIcon.style.display = 'none'; // Hide the search-bar-icon
+	  handleResize(); // Update the visibility of the search icon on resize
 	});
-}
+ 
+	// When the window is resized, call handleResize function
+	window.addEventListener('resize', handleResize);
+ 
+	document
+	  .querySelector('.header__menu-button')
+	  .addEventListener('click', function () {
+		 document
+			.querySelector('.header__nav')
+			.classList.toggle('header__nav--active');
+	});
+ } 
