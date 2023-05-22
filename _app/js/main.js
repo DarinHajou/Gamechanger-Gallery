d98aded5972e5ab765af76478d3553122b9c52d7
Darin Hajou <it_related@hotmail.com>
2023-05-22 16:48:50 +0200

    Add latest transformation slideshow section

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import header from './modules/header.js';
 import bannerSlide from './modules/banner-slide.js';
+import transformSlideshow from './modules/transformation-slideshow.js'
 
 header();
 bannerSlide();
+transformSlideshow();
