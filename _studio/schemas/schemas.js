1e421b64e6acd7ad2dcddab33becb328789f2577
Darin Hajou <it_related@hotmail.com>
2023-05-26 16:26:26 +0200

    Add image styles to transformationImages schema (#18)

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,4 +1,5 @@
 import sport from './documents/sport.js';
 import athletes from './documents/athletes.js';
+import transformationImages from './documents/transformationImages.js';
 
-export default [sport, athletes];
+export default [sport, athletes, transformationImages];
