69368bf14297a13a53a9ea1ebce74ace6530fc8e
Darin Hajou <it_related@hotmail.com>
2023-05-28 18:48:00 +0200

    Update schema and grid layout (#22)

--- _app/js/modules/displayTransformedImages.js
+++ _app/js/modules/displayTransformedImages.js
@@ -1,32 +1,21 @@
-import fetchTransformedImages from './fetchTransformedImages.js';
+//displayTransformationImages.js
 
-export default function displayTransformedImages() {
-  // Function to create HTML elements for each transformed image
-  function createTransformedImageElements(image) {
-    // Create an anchor element for the image
-    const imageLink = document.createElement('a');
-    imageLink.href = `path/to/athlete-details.html?id=${image.athlete._id}`;
+	export default function () { 
 
-    // Create an image element
-    const imgElement = document.createElement('img');
-    imgElement.src = image.image.asset.url;
-    imgElement.alt = image.name;
-
-    // Append the image element to the anchor element
-    imageLink.appendChild(imgElement);
-
-    return imageLink;
-  }
-
-  // Assuming you have a container element with the id "transformationImagesContainer" in your HTML
-  const transformationImagesContainer = document.getElementById('transformationImagesContainer');
-
-  // Fetch the transformation images data
-  fetchTransformedImages().then((data) => {
-    // Iterate over the fetched data and create HTML elements for each transformed image
-    data.forEach((image) => {
-      const transformedImageElement = createTransformedImageElements(image);
-      transformationImagesContainer.appendChild(transformedImageElement);
-    });
-  });
-}
+		// Function to create HTML elements for each transformed image
+		function createTransformedImageElements(image) {
+		// Create an anchor element for the image
+		const imageLink = document.createElement('a');
+		imageLink.href = `path/to/athlete-details.html?id=${image.athlete._id}`;
+		
+		// Create an image element
+		const imgElement = document.createElement('img');
+		imgElement.src = image.image.asset.url;
+		imgElement.alt = image.name;
+		
+		// Append the image element to the anchor element
+		imageLink.appendChild(imgElement);
+		
+		return imageLink;
+		}	
+	}
