b440c9e56497c07861f21d0e8161e1653322904f
Darin Hajou <it_related@hotmail.com>
2023-05-28 18:42:19 +0200

    Update schema and grid layout

--- _app/js/main.js
+++ _app/js/main.js
@@ -4,9 +4,11 @@ import bannerSlide from './modules/banner-slide.js';
 import transformSlideshow from './modules/transformation-slideshow.js';
 import fetchImageData from './modules/fetchImageData.js';
 import displayTransformedImages from './modules/displayTransformedImages.js';
+import fetchTransformedImages from './modules/fetchTransformedImages.js';
 
 header();
 bannerSlide();
 transformSlideshow();
 fetchImageData();
 displayTransformedImages();
+fetchTransformedImages();
--- _app/js/modules/fetchTransformedImages.js
+++ _app/js/modules/fetchTransformedImages.js
@@ -1,3 +1,5 @@
+//fetchTransformedImages.js
+
 import { sanity } from "../sanity.js";
 
 export default async function fetchTransformedImages() {
@@ -13,15 +15,66 @@ export default async function fetchTransformedImages() {
     athlete->{
       _id,
       name
-    }
+    },
+    imageStyle->{_id, description}
   }`;
 
   try {
-    const data = await sanity.fetch(query);
+    const data = await sanity.fetch(query, { expandRefs: true });
     console.log("Fetched data:", data);
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
   }
+}
+
+const transformationImagesContainer = document.getElementById(
+  'transformationImagesContainer'
+);
+
+fetchTransformedImages().then((data) => {
+  data.forEach((image) => {
+    const transformedImageElement = createTransformedImageCard(image);
+    console.log("Image:", image);
+    transformationImagesContainer.appendChild(transformedImageElement);
+  });
+});
+
+function createTransformedImageCard(image) {
+  const card = document.createElement('div');
+  card.classList.add('card', 'grid__column--3');
+
+  const imageLink = document.createElement('a');
+  imageLink.href = `path/to/athlete-details.html?id=${image.athlete._id}`;
+
+  const imgElement = document.createElement('img');
+  imgElement.src = image.image.asset.url;
+  imgElement.alt = image.name;
+
+  imageLink.appendChild(imgElement);
+  card.appendChild(imageLink);
+
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('card__content');
+
+  const titleElement = document.createElement('h3');
+  titleElement.classList.add('card__title');
+  titleElement.textContent = image.name;
+
+  const descriptionElement = document.createElement('p');
+  descriptionElement.classList.add('card__description');
+  descriptionElement.textContent = '';
+
+  // Check if imageStyle exists and has description
+  const imageStyle = image.imageStyle || {};
+  console.log("ImageStyle:", imageStyle);
+  const description = image.imageStyle && image.imageStyle.description ? image.imageStyle.description : 'No description available';
+  console.log("Description:", description);
+
+  cardContent.appendChild(titleElement);
+  cardContent.appendChild(descriptionElement);
+  card.appendChild(cardContent);
+
+  return card;
 }
