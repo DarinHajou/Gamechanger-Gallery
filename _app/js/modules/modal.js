97bc76c34ec843306fd8d0050b02947bdc2c0ff3
Darin Hajou <it_related@hotmail.com>
2023-05-31 11:27:57 +0200

    Create responsive spotlight section

--- _app/js/modules/modal.js
+++ _app/js/modules/modal.js
@@ -2,19 +2,26 @@
 export default function createModal(description) {
 	const modal = document.createElement('div');
 	modal.classList.add('modal');
- 
+
 	const modalContent = document.createElement('div');
 	modalContent.classList.add('modal-content');
- 
+
+	const descriptionLink = document.createElement('a');
+	descriptionLink.href = 'path/to/athlete-details.html';
+	descriptionLink.target = '_blank'; // Open the link in a new tab/window
+	descriptionLink.style.textDecoration = 'none'; // Remove link decoration
+
 	const descriptionText = document.createElement('p');
 	descriptionText.textContent = description || 'Image description or additional information';
- 
-	modalContent.appendChild(descriptionText);
+
+	descriptionLink.appendChild(descriptionText);
+	modalContent.appendChild(descriptionLink);
+
 	modal.appendChild(modalContent);
- 
+
 	// Add event listener to show modal on hover
 	modal.addEventListener('mouseenter', () => {
-	  modal.classList.add('show');
+		modal.classList.add('show');
 	});
  
 	// Add event listener to hide modal on mouseleave
