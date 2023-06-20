390b6b9ee97dfb689bd1f7a49e2c279f9f95092d
Darin Hajou <it_related@hotmail.com>
2023-06-20 15:59:03 +0200

    Delete displayTransformedImages.js from directory

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,7 +2,6 @@
 import header from './modules/header.js';
 import bannerSlide from './modules/banner-slide.js';
 import transformSlideshow from './modules/transformation-slideshow.js';
-import displayTransformedImages from './modules/displayTransformedImages.js';
 import fetchTransformedImages from './modules/fetchTransformedImages.js';
 import createModal from './modules/modal.js';
 import fetchAthleteDetails  from './modules/fetchAthleteDetails.js';
@@ -11,7 +10,6 @@ import displayAthleteDetails  from './modules/displayAthleteDetails.js';
 header();
 bannerSlide();
 transformSlideshow();
-displayTransformedImages();
 fetchTransformedImages();
 createModal();
 fetchAthleteDetails();
