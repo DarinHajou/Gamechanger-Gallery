58066416c9b893d635da40150a2dcece90dfd0f0
Darin Hajou <it_related@hotmail.com>
2023-07-11 16:52:50 +0200

    Update import in main for bannerSlide.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,7 @@
 // main.js
 import header from './modules/header.js';
-import bannerSlide from './modules/banner-slide.js';
-import transformSlideshow from './modules/transformation-slideshow.js';
+import bannerSlide from './modules/bannerSlide.js';
+import transformSlideshow from './modules/transformationSlideshow.js';
 import fetchTransformedImages from './modules/fetchTransformedImages.js';
 import createModal from './modules/modal.js';
 import fetchAthleteDetails from './modules/fetchAthleteDetails.js';
